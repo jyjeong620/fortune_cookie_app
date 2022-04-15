8f71418b840103a27caa1693907981ce0b4b7774
정준영 <jeongjy@mrblue.com>
2022-04-15 12:31:37 +0900

    feat: 버튼이 눌렸을경우 멘트가 나올수있도록 추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,18 +27,19 @@ class _HomePageState extends State<HomePage> {
         title: const Center(child: Text("Fortune Cookie")),
       ),
 
-      // TODO: 17. 버튼이 클릭되면 텍스트 메시지가 업데이트되도록 처리해주세요. (Hint! 상태 갱신은 setState 메서드를 사용하세요.)
       body: Column(children: [
         Image.asset("assets/images/fortune-cookie.jpg"),
-        const Text(
-          "텍스트 입니다 ",
-          style: TextStyle(fontSize: 18),
+        Text(
+          _cookie.message,
+          style: const TextStyle(fontSize: 18),
         ),
         SizedBox(
           width: 300,
           height: 50,
           child: MaterialButton(
-            onPressed: () {},
+            onPressed: () => setState(() {
+              _cookie = _cookieBox.pick;
+            }),
             color: Colors.red,
             child: const Text(
               "Lucky!",

fb651fe6dc74268bf236cd081597017d13b7c077
정준영 <jeongjy@mrblue.com>
2022-04-15 12:32:31 +0900

    feat: debug 라벨 제거

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'home_page.dart';
 
 void main() {
   runApp(const MaterialApp(
-    // TODO: 18. DEBUG 배너를 제거해주세요. (Hot reload가 안되면 앱을 재시작해주세요.)
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
 }
